9ac587fd996a905997ea70598b493d34d3928763
Nathan Jordan <natedagreat27274@gmail.com>
2013-11-20 03:35:05 +0000

    coupled with documentation

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, send_file
 
 app = Flask(__name__)
 
@@ -9,9 +9,22 @@ app.debug = True
 def homepage():
 	return render_template('index.html')	
 
+# Serves user documentation
+@app.route('/docs/')
+def serve_documentation_homepage():
+	# Return index page
+	return send_file('static/docs/index.html')
+
+@app.route('/docs/<path:resource>')
+def serve_documentation(resource):
+    # Return the static file
+    if resource[len(resource)-1:] == '/':
+        resource = resource + 'index.html'
+    return send_from_directory('static/docs/', resource)
+
 # Serves static resources like css, js, images, etc.
 @app.route('/assets/<path:resource>')
-def serveStaticResource(resource):
+def serve_static_content(resource):
     # Return the static file
     return send_from_directory('static/assets/', resource)
 
